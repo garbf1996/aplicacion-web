b84bdebec92deb3d218098821af1065f1cf89e1f
garbf1996 <garbfbatista@gmail.com>
2022-09-05 14:32:33 -0400

    configuracion de pdf

--- secciones/certificado.php
+++ secciones/certificado.php
@@ -3,12 +3,12 @@ require('../pdf/fpdf.php');
 include_once '../config/bd.php';
 $conexion=BD::crearInstancia();
 
-function agregarTexto($pdf,$texto,$posicionHorizontal,$posicionVertical,$alineacion,$f,$t,$s,$r,$g,$b) 
-{
-    $pdf->SetFont($f,$t,$s);	
-    $pdf->SetXY($posicionHorizontal,$posicionVertical);
+
+function agregarTexto($pdf,$texto,$x,$y,$aling='L',$fuente,$size=10,$r=0,$g=0,$b=0){
+    $pdf->SetFont($fuente,"",$size);	
+    $pdf->SetXY($x,$y);
     $pdf->SetTextColor($r,$g,$b);
-    $pdf->Cell(0,10,$texto,0,0,$alineacion);	
+    $pdf->Cell(0,10,$texto,0,0,$aling);	
 }
 
 function AgregarImagen($pdf, $imagen, $x, $y) {
@@ -29,13 +29,16 @@ $stmt->bindParam(':idcurso',$idcurso);
 $stmt->execute();
 $alumno=$stmt->fetch(PDO::FETCH_ASSOC);
 
+//print_r($cursosAlumno);
 $pdf = new FPDF('L','mm',array(254,190));
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 AgregarImagen($pdf,'../img/certificado_.jpg', 0,0);
-
-
+agregarTexto($pdf,"Garber",60,70,'L','Helvetica',30,0,84,115);
+agregarTexto($pdf,"Sitio WEB",-190,115,'c','Helvetica',20,0,84,115);
+agregarTexto($pdf,"01/01/2022",-190,156,'c','Helvetica',11,0,84,115);
 $pdf->Output();
+
 
 
 ?>
